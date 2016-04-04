19df357b930a40f182258b35c6f1f2f112fad0ab
Miliko <aberrau@gmail.com>
2016-04-04 17:43:34 +0100

    second commit

--- text_101/Assets/TextController.cs
+++ text_101/Assets/TextController.cs
@@ -2,21 +2,78 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
 public class TextController : MonoBehaviour {
 	public Text text;
+	private enum State {cell, sheets, mirror, lock_0, cell_mirror};
+	private State current_state;
 	// Use this for initialization
 	void Start () {
-		text.text = "";
+		current_state = State.cell;
 	}
 	
 	// Update is called once per frame
 	void Update ()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{ text.text = "Oh my god, what the hell am I doing here? " +
-		              "I must escape right now. There are only " +
-		              "some dirty sheets and a mirror on the wall.\n\n " +
-		              "Shit !! the door in front of me is locked.";
+	{	if (current_state == State.cell)
+		{	cell_method ();
+		}
+		else if (current_state == State.sheets)
+		{	sheets_method ();
+		}
+		else if (current_state == State.lock_0)
+		{	lock_0_method ();
+		}
+		else if (current_state == State.mirror)
+		{	mirror_method ();
+		}
+		else if (current_state == State.cell_mirror)
+		{	cell_mirror_method ();
 		}
 	}
+	
+	void cell_method ()
+		{	text.text = "You are locked at the cell. Press S to inspect the sheets, M to inspect the mirror or L to inspect the lock";
+			if (Input.GetKeyDown(KeyCode.S))
+			{	current_state = State.sheets;
+			}
+			else if (Input.GetKeyDown(KeyCode.M))
+			{	current_state = State.mirror;
+			}
+			else if (Input.GetKeyDown(KeyCode.L))
+			{	current_state = State.lock_0;
+			}
+		}
+
+	void sheets_method ()
+	{	text.text = "Your sheets are dirty. Press R to return";				
+		if (Input.GetKeyDown(KeyCode.R))
+		{	current_state = State.cell;
+		}
+	}
+	
+	void mirror_method ()
+	{	text.text = "You are at the mirror. It is broken. Press T to take a broken glass, press R to return";
+		if (Input.GetKeyDown(KeyCode.T))
+		{	current_state = State.cell_mirror;
+		}
+		else if (Input.GetKeyDown(KeyCode.R))
+		{	current_state = State.cell;
+		}
+	}
+	
+	void lock_0_method ()
+	{	text.text = "It is closed from outside. Maybe you could try to force it with some tools. Press R to return";
+		if (Input.GetKeyDown(KeyCode.R))
+		{	current_state = State.cell;
+		}
+	}
+	
+	void cell_mirror_method ()
+	{	text.text = "You hold the mirror glass with your hands. Press R to return";
+		if (Input.GetKeyDown(KeyCode.R))
+		{	current_state = State.cell;
+		}
+	}
+	
 }
+
